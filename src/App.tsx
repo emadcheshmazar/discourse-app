f80e95919d2208c67093c050666dfc1244e8941d
Emad <e.cheshmazar@dariahamrah.ir>
2025-10-25 07:33:34 +0330

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 
         <main>
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/discourse-landing-build/" element={<Home />} />
           </Routes>
         </main>
       </div>
